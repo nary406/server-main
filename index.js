35e060c2f26067ff8a3c8c249714a2e4f30f2cb1
nary406 <naryxyz3@example.com>
2024-10-29 11:49:45 +0530

    add cors header manually

--- index.js
+++ index.js
@@ -26,7 +26,31 @@ const corsOptions = {
 
 expressApp.use(cors(corsOptions));
 
-expressApp.options('*', cors(corsOptions));
+
+
+
+
+// Handle preflight OPTIONS requests across all routes
+expressApp.options('*', (req, res) => {
+    res.set({
+        'Access-Control-Allow-Origin': allowedOrigins.includes(req.headers.origin) ? req.headers.origin : '',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+    });
+    res.sendStatus(200);
+});
+
+// Manually add CORS headers for non-preflight requests in case Vercel omits them
+expressApp.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigins.includes(req.headers.origin) ? req.headers.origin : '');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+
 
 expressApp.use('/', require('./routes/userRoutes')); 
 expressApp.use('/admin', require('./routes/dataRoutes'));
